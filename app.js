e6cdf1b2f9d4cb0078518c2620d2d4cb3d7b9014
John Yates <john@locbit.com>
2017-03-22 14:44:16 -0700

    Constructors and generators

--- app.js
+++ app.js
@@ -1,44 +1,156 @@
-var Sequelize = require("sequeilize");
+/** https://en.wikipedia.org/wiki/Semantic_primes **/
+
+var Sequelize = require("sequelize");
+var NodeNeuralNetwork = require('node-neural-network'); // this line is not needed in the browser
+
 var sqlz = new Sequelize();
 
-sqlz.define("NounRoots", {});
-sqlz.define("Nouns", {});
-sqlz.define("VerbRoots", {});
-sqlz.define("Verbs", {});
-sqlz.define("AdjectiveRoots", {});
-sqlz.define("Adjectives", {});
+var Neuron = NodeNeuralNetwork.Neuron,
+    Layer = NodeNeuralNetwork.Layer,
+    Network = NodeNeuralNetwork.Network,
+    Trainer = NodeNeuralNetwork.Trainer,
+    Architect = NodeNeuralNetwork.Architect;
 
-sqlz.define("Syntax", {});
+sqlz.define("Roots", {
+    type: {},
+    morpheme: {}
+});
 
-function start() {
-    var core = {
-        Verbs: {
-            toBe: generateRandomAlpha(3),
-            toGo: generateRandomAlpha(3),
-            toCome: generateRandomAlpha(3),
-            toSpeak: generateRandomAlpha(3),
-            toConsume: generateRandomAlpha(3),
-            toRest: generateRandomAlpha(3)
-        },
-        Nouns: {
-            Person: generateRandomAlpha(6),
-            Object: generateRandomAlpha(6),
-            TimeUnit: generateRandomAlpha(6),
-            Consumable: generateRandomAlpha(6)
-        },
-        Adjectives: {
-            Good: generateRandomAlpha(8),
-            Bad: generateRandomAlpha(8),
-            More: generateRandomAlpha(8),
-            Less: generateRandomAlpha(8)
+sqlz.define("Words", {
+    type: {},
+    root: {},
+    morpheme: {},
+    modifier: {}
+});
+
+sqlz.define("Modifiers", {
+
+});
+
+function Word(type, root, morpheme, modifier) {
+    this.type = type;
+    this.root = root;
+    this.morpheme = morpheme;
+    this.modifier = modifier;
+}
+
+function Clause(structure, subject, verb, object) {
+    this.content = [];
+
+    for(var s in structure) {
+        if(structure[s] == "subject") {
+            this.content[s] = subject;
+        } else if(structure[s] == "object") {
+            this.content[s] = object;
+        } else {
+            this.content[s] = verb+" ";
+        }
+    }
+}
+
+function Sentence(clauses, junctions) {
+    this.content = [];
+
+    if(clauses.length < junctions.length) {
+        throw new Error("Invalid Sentence structure.");
+    } else {
+        for(var c in clauses) {
+            if(junctions[c]) {
+                this.content[c] = clauses[c]+junctions[c]+" ";
+            } else {
+                this.content[c] = clauses[c]+" ";
+            }
+        }
+    }
+}
+
+function Language() {
+    this.alphabet = null;
+    this.structure = null;
+    this.verbs = {};
+    this.nouns = {};
+
+    this.createAlphabet = function() {
+        var alphabet = Math.random().toString(36).replace(/[0-9.]+/g, '').split('');
+        var filtered = [];
+
+        for(var letter in alphabet) {
+            if(filtered.indexOf(alphabet[letter]) == -1) {
+                filtered.push(alphabet[letter]);
+            }
+        }
+
+        this.alphabet = filtered;
+    };
+
+    this.createVerbs = function() {
+        var keys = ["toBe", "toGo", "toDo", "toEat", "toDrink", "toLive", "toDie", "toHeal", "toKill"];
+
+        for(var k in keys) {
+            this.verbs[k] = this.createWord(keys[k], "Verb");
+
+            for(var v in this.verbs) {
+                if(this.verbs[k] == this.verbs[v] && v != k) {
+                    this.verbs[v] = this.createWord(keys[k], "Verb");
+                }
+            }
         }
     };
 
-    console.log(core);
-}
+    this.createNouns = function() {
+        var keys = ["Person", "Food", "Beverage"];
 
-function generateRandomAlpha(lim) {
-    return Math.random().toString(36).replace(/[0-9.]+/g, '');
-}
+        for(var k in keys) {
+            this.nouns[k] = this.createWord(keys[k], "Noun");
 
-start();
+            for(var n in this.nouns) {
+                if(this.verbs[k] == this.verbs[n] && n != k) {
+                    this.verbs[n] = this.createWord(keys[k], "Noun");
+                }
+            }
+        }
+    };
+
+    this.createWord = function(root, type) {
+        var length = Math.floor((Math.random()+0.5) * 5);
+        var word = "";
+
+        for(var i=0; i<length;i++) {
+            word += this.alphabet[Math.floor(Math.random() * this.alphabet.length)];
+        }
+
+        return new Word(type, root, word, null);
+    };
+
+    this.createClause = function(subject, verb, object) {
+        return new Clause(this.structure, subject, verb, object);
+    };
+
+    this.createSentence = function(clauses, junctions) {
+        return new Sentence(clauses, junctions);
+    };
+
+    this.createSyntaxRules = function() {
+        this.structure = ["subject", "object", "verb"];
+
+        var j, x, i;
+        for (i = this.structure.length; i; i--) {
+            j = Math.floor(Math.random() * i);
+            x = this.structure[i - 1];
+            this.structure[i - 1] = this.structure[j];
+            this.structure[j] = x;
+        }
+    };
+
+    this.createAlphabet();
+    this.createSyntaxRules();
+    this.createVerbs();
+    this.createNouns();
+
+    console.log(this.structure);
+    console.log(this.verbs);
+    console.log(this.nouns);
+};
+
+
+module.exports = new Language();
